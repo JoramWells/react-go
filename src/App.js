50266f116067eff6e8676ba751efd738c22d8f70
bramuel <jorammanoah1@gmail.com>
2021-02-24 07:04:18 -0800

    Initial commit

--- src/App.js
+++ src/App.js
@@ -2,11 +2,7 @@ import React, {Component} from "react";
 import "./App.css";
 import './bootstrap/css/bootstrap.min.css'
 
-import NavBar from './Components/NavBar'
-import NavHeader2 from "./Components/NavHeader2";
-import Taskbar from "./Components/Taskbar";
-import Footer2 from './Components/Footer2'
-import Certificates from "./Components/Certificates";
+import Header from "./Components/App/Header";
 
 
 
@@ -14,6 +10,7 @@ class App extends Component {
   render() {
     return (
       <div>
+        <Header />
 
       </div>
 
