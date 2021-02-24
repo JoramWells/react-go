ade6761babf9cfce1489194ce0b654e29af25f70
bramuel <jorammanoah1@gmail.com>
2021-02-24 06:54:37 -0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, {Component} from "react";
+import "./App.css";
+import './bootstrap/css/bootstrap.min.css'
+
+import NavBar from './Components/NavBar'
+import NavHeader2 from "./Components/NavHeader2";
+import Taskbar from "./Components/Taskbar";
+import Footer2 from './Components/Footer2'
+import Certificates from "./Components/Certificates";
+
+
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+
+      </div>
+
+    );
+  }
+}
+
+export default App;
